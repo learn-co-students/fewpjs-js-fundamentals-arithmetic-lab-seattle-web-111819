ab895a35cd94723d5d5c006a10bafcd3a8314843
4myc <missch3n@gmail.com>
2020-01-14 15:31:35 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+// 1. multiply - an equation whose multiplied results will equal 62
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+// 2. random - generates a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1
+/* Math.random() returns a number between 0 and 0.999..., 
+which we multiply by 10 to give us a number between 0 and 9.999.... 
+We then pass that number to Math.floor(),
+which returns an integer between 0 and 9. */
+
+// 3. mod - generates a remainder that is equal to 4:
+const mod = Number.parseInt('100', 2)
+/* parseInt() Accepts a string as its first argument and parses it as an integer. 
+The second argument is the base that should be used in parsing (e.g., 2 for binary or 10 for decimal) */
+
+// 4. max - will return 20 as the highest number in the set
+const max = Math.max(10, 15, 20)
