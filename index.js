efdb69ec27bc51625fdf4c6ac14bb7f0d43e312b
NicoleLC16 <asalwaysnicole@outlook.com>
2020-01-15 12:43:46 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.random();
+
+let mod = 44 % 10
+
+let max = Math.max(15, 16, 17, 18, 19, 20)
